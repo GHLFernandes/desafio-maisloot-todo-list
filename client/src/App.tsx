9186ea90531ef3fb3c13075bb06de6ba4fc7907c
Gabriel Fernandes <fernandes.gh@outlook.com>
2023-04-06 01:12:46 -0300

    feat: display data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,73 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import TaskItem from './components/TaskItem';
+import AddTask from './components/AddTask';
+import { getTasks, addTask, updateTask, deleteTask } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = (): void => {
+    getTasks()
+      .then(({ data: { tasks } }: ITask[] | any ) => setTasks(tasks))
+      .catch((error: Error) => console.log(error));
+  };
+
+  const handleSaveTask = (e: React.FormEvent, dataForm: ITask): void => {
+    e.preventDefault();
+
+    addTask(dataForm)
+      .then(({ status, data }) => {
+        if(status !== 201){
+          throw new Error('Error! Task not saved!');
+        }
+        setTasks(data.tasks);
+      })
+      .catch(error => console.log(error));
+  };
+
+  const handleUpdateTask = (task: ITask): void => {
+    updateTask(task)
+      .then(({status, data}) => {
+        if(status !== 200){
+          throw new Error('Error! Task not updated!');
+        }
+      setTasks(data.tasks);
+      })
+      .catch(error => console.log(error));
+  };
+
+  const handleDeleteTask = (_id: string): void => {
+    deleteTask(_id)
+      .then(({status, data}) => {
+        if(status !== 200){
+          throw new Error('Error! Task not deleted!');
+        }
+
+        setTasks(data.tasks);
+      })
+      .catch(error => console.log(error));
+  };
+
+  return(
+    <main className='App'>
+      <h1>My Tasks</h1>
+      <AddTask saveTask={handleSaveTask} />
+      {
+        tasks.map((task: ITask) => (
+          <TaskItem
+            key={task._id}
+            updateTask={handleUpdateTask}
+            deleteTask={handleDeleteTask}
+            task={task}
+          />
+        ))
+      }
+    </main>
+  )
 }
 
 export default App;
